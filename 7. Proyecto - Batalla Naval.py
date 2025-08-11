7704848c673c557979ef61593c7763c1eed6871a
agustin-arg <agustinalbertoleiva@gmail.com>
2025-08-10 22:51:04 -0300

    Add initial implementation of Ship class and project description for Battleship game

---
+++ 7. Proyecto - Batalla Naval.py
@@ -0,0 +1,8 @@
+class Ship:
+    def __init__(self,name,size):
+        self.name = name
+        self.size = size
+        self.positions = []
+        self.hits = 0
+    def place_ship(self):
+        pass
