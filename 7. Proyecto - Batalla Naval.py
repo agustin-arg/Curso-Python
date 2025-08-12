10f74acd7e2e333f3c74c49c3ce8f6457669ec0e
agustin-arg <agustinalbertoleiva@gmail.com>
2025-08-12 11:52:08 -0300

    actualizacion proyecto

--- 7. Proyecto - Batalla Naval.py
+++ 7. Proyecto - Batalla Naval.py
@@ -4,5 +4,38 @@ class Ship:
         self.size = size
         self.positions = []
         self.hits = 0
-    def place_ship(self):
-        pass
+    def place_ship(self, start_row, start_col, direction, board):
+        positions = []
+        if direction == 'H':
+            if start_col + self.size > len(board[0]):
+                return False
+            else:
+                for i in range(self.size):
+                    if board[start_row,start_col+i] != ' ':
+                        return False
+                    
+            
+    def hit(self):
+        self.hits += 1
+        return self.hits == self.size
+        
+class Destroyer(Ship):
+    def __init__(self):
+        super().__init__('destructor', 2)
+
+class Submarine(Ship):
+    def __init__(self):
+        super().__init__('Submarino', 3)
+        
+class Battleship(Ship):
+    def __init__(self):
+        super().__init__('Acorazado', 4)
+        
+class Player():
+    def __init__(self,name):
+        self.name = name
+        self.board = [[' ' for _ in range(10)] for _ in range(10)]
+        self.list_ships = []
+        self.histboard = [[' ' for _ in range(10)] for _ in range(10)]
+    
+        
